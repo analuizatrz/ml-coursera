c7150696ffb2476a4791e3eda2f928f12aa05bd8
Ana Luiza Sanches <analuizatrz@gmail.com>
2020-08-26 20:42:11 -0300

    ex1 2.2.4 Gradient Descent

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -17,12 +17,10 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
-
+    %vectorization
+    dJ = (1/m)*(theta'*X'-y')*X;
+    theta = theta - alpha*dJ';
+    
     % ============================================================
 
     % Save the cost J in every iteration    
